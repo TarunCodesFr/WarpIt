5c6fab485087be6b1e071d83a26b54466a9eb90d
taruncodesfr <akurax4@gmail.com>
2026-04-19 21:03:56 +0530

    chore: rate limiter in link routes

--- shortner_api/src/core/modules/v1/links/link.route.ts
+++ shortner_api/src/core/modules/v1/links/link.route.ts
@@ -1,9 +1,9 @@
 import {Router} from 'express';
 import {createCustomRedirectLink, createRedirectLink, redirectToOriginalLink} from './link.controller';
+import {limiter} from '../../../../utils/rateLimiter';
 
 const router = Router();
-
-router.post('/create-link', createRedirectLink);
-router.get('/getlink/:shortCode', redirectToOriginalLink);
-router.post('/create-custom-link', createCustomRedirectLink);
+router.post('/create-link', limiter, createRedirectLink);
+router.get('/getlink/:shortCode', limiter, redirectToOriginalLink);
+router.post('/create-custom-link', limiter, createCustomRedirectLink);
 export default router;
